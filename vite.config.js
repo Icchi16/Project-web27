1a1c8639b7f8bd937541661e2812dcbaf721dc8e
Icchi16 <duynghiemhn16@gmail.com>
2022-10-07 19:17:50 +0700

    áds

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "./src",
   build: {
+    outDir: "dist",
     rollupOptions: {
       input: {
         index: resolve(__dirname, "src/index.html"),
-        about: resolve(__dirname, "src/about.html"),
-        products: resolve(__dirname, "src/products.html"),
+        // about: resolve(__dirname, "src/about.html"),
+        // products: resolve(__dirname, "src/products.html"),
       },
     },
   },
