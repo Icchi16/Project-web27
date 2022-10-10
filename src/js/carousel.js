98b1507a16220a03b5e2230be4d46f18a5702b25
Icchi16 <duynghiemhn16@gmail.com>
2022-10-10 23:24:51 +0700

    Carousel Libary Sucked!

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -4,6 +4,3 @@ import "../css/hot-carousel.css";
 
 import $ from "jquery";
 $(".hero-carousel").load("../components/hero-carousel.html");
-$(".hot-carousel").load("../components/hot-carousel.html", function () {
-  $(".hot-slider").slick();
-});
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,11 @@
 // import dependencies
-import "jquery/dist/jquery.min.js";
 import $ from "jquery";
-
+import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.js";
-// import "jquery-migrate/dist/jquery-migrate.min.js";
-import Glide from "@glidejs/glide";
-import "slick-carousel/slick/slick.min.js";
 
 // import css
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "@glidejs/glide/dist/css/glide.core.min.css";
-import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
 
 import "../css/global.css";
 import "../css/index.css";
@@ -20,9 +13,3 @@ import "../css/index.css";
 // import components
 import "./header.js";
 import "./carousel.js";
-
-// new Glide(".glide", {
-//   type: "carousel",
-//   perView: 1,
-//   focusAt: "center",
-// }).mount();
