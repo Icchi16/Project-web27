db535e5dede1f5ef4e6165ed063869bcec6ebb40
Icchi16 <duynghiemhn16@gmail.com>
2022-10-17 23:22:49 +0700

    css event.html

--- src/js/event.js
+++ src/js/event.js
@@ -1,5 +1,6 @@
 // import css
-import "../css/index.css";
+import "../css/event.css";
+import "../css/hero-carousel.css";
 
 // import dependencies
 import $ from "jquery";
@@ -7,3 +8,16 @@ import $ from "jquery";
 // import components
 import "./header.js";
 import "./footer.js";
+
+$(function () {
+  $(".accordion-button").on("click", function (e) {
+    e.preventDefault();
+
+    if ($(this).find(".plus").hasClass("minus")) {
+      $(this).find(".plus").toggleClass("minus");
+    } else {
+      $(this).closest(".accordion").find(".plus").removeClass("minus");
+      $(this).find(".plus").addClass("minus");
+    }
+  });
+});
--- src/js/owlcarousel.js
+++
@@ -1,4 +0,0 @@
-import $ from "./jquery.js";
-import "owl.carousel/dist/owl.carousel.min.js";
-import "owl.carousel/dist/assets/owl.carousel.min.css";
-
